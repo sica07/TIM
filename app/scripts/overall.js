edd21c0ddba52741f65699cfbc43989485da0495
Marius Matei <mariusmatei31@gmail.com>
2014-03-25 16:00:24 +0200

    Added nice color scheme for charts to fit in the extension colorscheme
    Added an icon for the settings page

--- app/scripts/overall.js
+++ app/scripts/overall.js
@@ -9,8 +9,8 @@ function drawChart() {
     var options = {
         width: 600,
         height: 400,
-        legend: { position: 'top', maxLines: 3 },
         bar: { groupWidth: '75%' },
+        legend: { position: 'none'},
         isStacked: true,
     };
     var chart = new google.visualization.ColumnChart(document.getElementById('overall_div'));
